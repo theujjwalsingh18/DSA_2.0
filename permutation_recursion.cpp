9460bdf859d5f889b3c5c9a4f4446af3c1b5a0b6
theujjwalsingh18 <theujjwalsinghh@gmail.com>
2025-06-11 07:58:55 +0530

    optimized solution of permutations (swaping) -recursion

--- permutation_recursion.cpp
+++ permutation_recursion.cpp
@@ -40,3 +40,35 @@ public:
 ----------------------------------------------------------------------------------
 
 */
+
+//  Optimizes Soultion of this problem using swaping to save extra space
+
+class Solution {
+public:
+    void swapPermute(int idx, vector<int> &arr, vector<vector<int>> &ans){
+        if ( idx == arr.size()){
+            ans.push_back(arr);
+            return;
+        }
+        for (int i = idx; i<arr.size(); i++){
+            swap(arr[idx], arr[i]);
+            swapPermute(idx+1, arr, ans);
+            swap(arr[idx], arr[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        swapPermute(0, nums, ans);
+        return ans;
+    }
+};
+
+/*
+
+--------------------- Time Complexity and Space Complexity -----------------------
+|                                                                                |
+|                    T.C : O(N! * N)            S.C : O(1)                       |
+|                                                                                |
+----------------------------------------------------------------------------------
+
+*/
