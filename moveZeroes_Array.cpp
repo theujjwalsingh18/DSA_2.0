b6001484d43e72d30fa2e9a5ad9c55fe0ab94fe3
theujjwalsingh18 <theujjwalsinghh@gmail.com>
2025-06-21 15:35:21 +0530

    moves zeroes to the end - Array

---
+++ moveZeroes_Array.cpp
@@ -0,0 +1,25 @@
+// Problem : https://leetcode.com/problems/move-zeroes/
+// ------------------  Solution -----------------------------
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
+    }
+};
+/*
+
+--------------------- Time Complexity and Space Complexity -----------------------
+|                                                                                |
+|                       T.C :  O(N)         S.C : O(1)                           |
+|                                                                                |
+----------------------------------------------------------------------------------
+
+*/
